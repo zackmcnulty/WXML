c2b20933367227c3459c897f475456b3f5dd33d9
Zachary McNulty <zmcnulty@uw.edu>
2019-11-14 12:58:32 -0800

    updated readme and added requirements.txt

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # WXML
 Washington Experimental Mathematics Laboratory (WXML) is a computational-oriented mathematics research lab targeted for undergraduates.
+My teams project this quarter focused on matching complexes. A matching on a graph G is some subset (possibly empty) of the edges of G such that
+no edges in the subset are adjacent (share a vertex). The matching complex is the collection of all these subsets and is in turn a simplicial complex.
+Simplicial complexes are more abstract mathematical structures studied in a more general setting than what we did here, but to put it simply a simplicial
+complex on a set X is a subset of the powerset of X that is closed under subsets. Namely, if S is a simplicial complex, and x is in S, then so are all subsets of x 
+(remember x is an element of the powerset of X and hence a set itself).
+
+You can find more information on [wikipedia](https://en.wikipedia.org/wiki/Simplicial_complex).
+
+
+Lots of our team's inspiration came from this [paper](https://arxiv.org/abs/1906.03328). 
+
+
+
+### Code Instructions
+
+First, start off by cloning this repository using git. Essentially, you just need to download git (software) from the internet
+and run `git clone https://github.com/zackmcnulty/WXML.git` in the terminal/command-line. This will create a folder in your local
+directory and you can access the code there. Also run `pip install -r requirements.txt` in the command line to download all the 
+python libraries I use in this code.
+
+I have tried to make this code as accessible as possible. The main functions provided here is findind/drawing matching complex
+given a graph G AND finding a graph that generates a given matching complex. All of the primary functions are found in the file
+`matching_complexes.py` but I have also created some helper scripts `find_complex_from_graph.py` and `find_graph_from_complex.py` 
+which have the obvious uses. In these helper files, you just need to specify some needed information (e.g. the edge list for the graph or
+the matching complex) and run them: they will do the rest. 
+
+If you want more flexibility in what you do, take a look at `matching_complexes.py`. You can import its methods into another python file
+and use them as you wish.
+
+Side note: I also created a script `linegraph.py` that plots the line graph of a given graph (specified in the file itself) if you find this useful.
---
+++ find_complex_from_graph.py
@@ -0,0 +1,94 @@
+'''
+
+This file is for finding and drawing the matching complex of a given graph. It will output to the terminal
+a string representing the matching complex and attempt to plot the complex (if it is at most 3D).
+
+'''
+
+from matching_complexes import *
+import networkx as nx
+
+# NOTE: Set parameters and define graph as edge list  ================================================================
+
+
+draw_2D = False # draws a 2D plot for matching complex rather than 3D
+
+
+# edge weights to give edges in (filled in) tetrahedron, triangles, or just plain edges in the
+# matching complex. These weights are used in the drawing algorithm. A higher weight makes a given
+# edge want to be shorter. Hence giving tetrahedrons a higher weight may encourage those edges to
+# be more structured and actually form a tetrahedron rather than just get flattened.
+tetra_weight = 1 
+tri_weight = 1
+other_weight = 1
+
+# Number of iterations to use in the drawing algorithm. The algorithm is stochastic (spring-force algorithm)
+# so more iterations may improve performance.
+iterations = 100
+
+
+'''
+This is where you can specify the graph G you want to find the matching complex of.
+Specify it as an edge list. Some examples can be found below.
+'''
+
+
+#edge_list = [(1,2), (2,3), (3,4), (1,4), (1,3), (2,4)] # K_4
+#edge_list = [(1,2), (2,3), (3,4), (4,5), (5,6), (6,1)] # C_6
+edge_list = [(1,2), (2,3), (3,4), (4,5), (5,6), (6,7), (7,1)] # C_7
+#edge_list = [(1,2), (2,3), (3,4), (4,5), (5,6), (6,7), (7,8), (8,1)] # C_8
+#edge_list=[(1,2), (3,4), (5,6), (7,8)] # 4 disjoint edges
+#edge_list=[(1,2), (3,4), (5,6), (7,8)] # 4 disjoint edges
+
+#K_k,n
+'''
+k=4
+n=3
+edge_list =[]
+for i in range(k):
+    for j in range(k, k+n):
+        edge_list.append((i,j))
+
+print(edge_list)
+'''
+
+#K_n
+'''
+n=6
+edge_list=[(i,j) for i,j in itl.combinations(list(range(n)), 2)]
+'''
+
+# ============================================================================================
+
+# NOTE: you don't need to modify anything down here :D 
+
+
+G = nx.Graph()
+G.add_edges_from(edge_list)
+
+edge_labels = {e:i+1 for i,e in enumerate(G.edges())}
+
+# draw the graph given by edge list
+draw_graph(G, edge_labels=edge_labels)
+
+maximal_matchings = find_matching_complex(G, edge_labels)
+
+print("Matching Complex is :", maximal_matchings)
+
+max_dim = max([len(m) for m in maximal_matchings])
+
+
+if max_dim > 4:
+    print("Cannot draw matching complex. Simplicial complex dimension is > 3")
+elif max_dim == 1 or max_dim == 0:
+    print("Matching complex is trivial.")
+else:
+    M_G, fill = make_matching_complex(maximal_matchings, tetra_weight=tetra_weight, tri_weight=tri_weight, other_weight=other_weight)
+
+    if draw_2D:
+        draw_2D_matching_complex(M_G,fill)
+
+    else:
+        draw_3D_matching_complex(M_G,fill, iterations = iterations)
+
+print("done!")
